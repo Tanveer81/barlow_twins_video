22ed28c948bf2823fa3442445b39d9d14bb45bbe
Tanveer81 <tanvir01719@gmail.com>
2021-08-06 01:18:33 +0200

    multicrop bug fix

--- yvos_dataset_4.py
+++ yvos_dataset_4.py
@@ -31,11 +31,11 @@ def generate_barlow_twin_annotations(img_path, meta_path, out_path, frame_dist):
             for frame in category['frames']:
                 frame_categories[frame].append(j)
 
-        number_of_pairs = int(len(img_paths)/frame_dist) * 100
+        number_of_pairs = int(len(img_paths) / frame_dist) * 100
         attempts = 3 * number_of_pairs
         while True:
-            start = random.randint(0, len(img_paths) - frame_dist-1)
-            end = random.randint(start + frame_dist, len(img_paths)-1)
+            start = random.randint(0, len(img_paths) - frame_dist - 1)
+            end = random.randint(start + frame_dist, len(img_paths) - 1)
             key = f'{video_id}/{frames[start]}.jpg'
             if key not in frame_pairs and set(frame_categories[frames[start]]) == set(frame_categories[frames[end]]):
                 frame_pairs.append((key, f'{video_id}/{frames[end]}.jpg'))
@@ -65,7 +65,7 @@ def debug_barlow_twin_annotations(pair_meta_path, img_path):
         pairs.append(frames)
     print(f'Total Time for loading Pairs : {(time.time() - start_time)} seconds')
     for i in range(10):
-        i = random.randint(0, len(pairs)-1)
+        i = random.randint(0, len(pairs) - 1)
         image = Image.open(os.path.join(img_path, pairs[i][0]))
         image2 = Image.open(os.path.join(img_path, pairs[i][1].rstrip()))
         visualize(image)
@@ -84,7 +84,7 @@ def save_bounding_boxes_for_barlow_twins(path):
         frame_key = frame['video_id'] + '_' + frame['frame_id']
         bboxes_anno = {}
         for anno in frame['annotations']:
-            anno_key = str(anno['category_id'])+'_'+anno['object_id']
+            anno_key = str(anno['category_id']) + '_' + anno['object_id']
             bboxes_anno[anno_key] = anno['bbox']
         bboxes_frame[frame_key] = bboxes_anno
         print(f'{i + 1}/{length} : {(time.time() - start_time)} seconds')
@@ -145,6 +145,7 @@ def visualize_bbox(image, bbox):
     cv2.rectangle(image, (bbox[0], bbox[1]), (bbox[2], bbox[3]), (255, 0, 0), 2)
     visualize(image)
 
+
 class YvosDateset(Dataset):
     def __init__(self, meta_path, img_path, transform, crop=False, increase_small_area=False):
         super().__init__()
@@ -186,85 +187,103 @@ class YvosDateset(Dataset):
         gaussImage = np.concatenate((gaussImage, gaussImage, gaussImage), axis=2)
         return gaussImage
 
-    def increase_area(self, box, img_size, scale=2):
-        dist_x = ((box[2] - box[0]) * (scale - 1)) / 2
-        dist_y = ((box[3] - box[1]) * (scale - 1)) / 2
+    def increase_area(self, box, img_size, scale=2, all_small=False):
+        dist_x = (box[2] - box[0])
+        dist_y = (box[3] - box[1])
 
-        if box[0] - dist_x < 0:
+        if all_small:
+            if dist_x < img_size[0] / 3:
+                scale_x = (img_size[0] / 3) / dist_x
+            if dist_y < img_size[1] / 3:
+                scale_y = (img_size[1] / 3) / dist_y
+        else:
+            scale_x = scale
+            scale_y = scale
+
+        if box[0] - (dist_x * scale_x / 2) < 0:
             box[0] = 0
-            box[2] = dist_x * 4
-        elif box[2] + dist_x > img_size[0]:
+            box[2] = dist_x * scale_x
+        elif box[2] + (dist_x * scale_x / 2) > img_size[0]:
             box[2] = img_size[0]
-            box[0] = img_size[0] - 4 * dist_x
+            box[0] = img_size[0] - dist_x * scale_x
         else:
-            box[0] = box[0] - dist_x
-            box[2] = box[2] + dist_x
+            box[0] = box[0] - dist_x * (scale_x - 1) / 2
+            box[2] = box[2] + dist_x * (scale_x - 1) / 2
 
-        if box[1] - dist_x < 0:
+        if box[1] - (dist_y * scale_y / 2) < 0:
             box[1] = 0
-            box[3] = dist_y * 4
-        elif box[3] + dist_y > img_size[1]:
+            box[3] = dist_y * scale_y
+        elif box[3] + (dist_y * scale_y / 2) > img_size[1]:
             box[3] = img_size[1]
-            box[1] = img_size[1] - 4 * dist_y
+            box[1] = img_size[1] - dist_y * scale_y
         else:
-            box[2] = box[2] - dist_y
-            box[3] = box[3] + dist_y
+            box[1] = box[1] - dist_y * (scale_y - 1) / 2
+            box[3] = box[3] + dist_y * (scale_y - 1) / 2
         return [int(b) for b in box]
 
     def box_area(self, box):
         return (box[2] - box[0]) * (box[3] - box[1])
 
-    def union_of_boxes(self, boxes, img_size):
+    def union_of_boxes(self, boxes, img_size, all_small=False):
         x_min = min([x[0] for x in boxes])
         x_max = max([x[2] for x in boxes])
         y_min = min([x[1] for x in boxes])
         y_max = max([x[3] for x in boxes])
         box = [x_min, y_min, x_max, y_max]
-        if self.box_area(box) < 0.33 * (img_size[0] * img_size[1]):
-            box = self.increase_area(box, img_size, 1.5)
+        image_area = (img_size[0] * img_size[1])
+        box_area = self.box_area(box)
+        if box_area < 0.33 * image_area:
+            scale = image_area / box_area
+            scale = np.sqrt(scale / 3)
+            box = self.increase_area(box, img_size, scale, all_small)
         return box
 
     def center_bbox(self, box, img_size):
-        img_center = (img_size[0]/2, img_size[1]/2)
-        box_center = ((box[2]+box[0])/2, (box[3]+box[1])/2)
+        img_center = (img_size[0] / 2, img_size[1] / 2)
+        box_center = ((box[2] + box[0]) / 2, (box[3] + box[1]) / 2)
         x_dist = img_center[0] - box_center[0]
         y_dist = img_center[1] - box_center[1]
-
         box[0] += x_dist
         box[2] += x_dist
         box[1] += y_dist
         box[3] += y_dist
-
         return [int(b) for b in box]
 
     def cut_image(self, image, boxes):
-        '''
-            type: blur/ noise
-        '''
         image_size = image.size
-        boxes = [box for box in boxes if not self.is_small_object(box)]
-        union_box = self.union_of_boxes(boxes, image_size)
-        rand = .35#random.random()
-        if self.box_area(union_box) < 0.5 * (image_size[0] * image_size[1]) and rand > 0.66:
-            print('union_box_center_crop')
+        boxes_temp = [box for box in boxes if not self.is_small_object(box)]
+        all_small = len(boxes_temp) == 0
+        if all_small:
+            gaussImage = image.filter(ImageFilter.GaussianBlur(np.random.randint(20, 50)))
+            union_box = self.union_of_boxes(boxes, image_size, all_small)
             image = image.crop(tuple(union_box))
-            return image
-        else:
-            if rand <= 0.33:
-                print('GaussianBlur')
-                gaussImage = image.filter(ImageFilter.GaussianBlur(np.random.randint(20,50)))
-                gaussImage = np.array(gaussImage)
-            elif 0.33 < rand <= 0.495:
-                print('add_gaussian_noise')
-                gaussImage = self.add_gaussian_noise(image.size)
-            else:
-                print('add_random_noise')
-                gaussImage = self.add_random_noise(image.size)
             image = np.array(image)
-            # box = self.center_bbox(box, image_size)
-            for box in boxes:
-                gaussImage[box[1]:box[3], box[0]:box[2]] = image[box[1]:box[3], box[0]:box[2]]
+            gaussImage = np.array(gaussImage)
+            gaussImage[union_box[1]:union_box[3], union_box[0]:union_box[2]] = image
             gaussImage = Image.fromarray(gaussImage)
+            # visualize(gaussImage)
+            return gaussImage
+        else:
+            boxes = boxes_temp
+            union_box = self.union_of_boxes(boxes, image_size)
+            rand = random.random()
+            if self.box_area(union_box) < 0.5 * (image_size[0] * image_size[1]) and rand > 0.66:
+                image = image.crop(tuple(union_box))
+                return image
+            else:
+                if rand <= 0.33:
+                    gaussImage = image.filter(ImageFilter.GaussianBlur(np.random.randint(20, 50)))
+                    gaussImage = np.array(gaussImage)
+                elif 0.33 < rand <= 0.495:
+                    gaussImage = self.add_gaussian_noise(image.size)
+                else:
+                    gaussImage = self.add_random_noise(image.size)
+
+                image = np.array(image)
+                # box = self.center_bbox(box, image_size)
+                for box in boxes:
+                    gaussImage[box[1]:box[3], box[0]:box[2]] = image[box[1]:box[3], box[0]:box[2]]
+                gaussImage = Image.fromarray(gaussImage)
             return gaussImage
 
     def __getitem__(self, index: int):
@@ -280,8 +299,8 @@ class YvosDateset(Dataset):
         # visualize(image1)
         # visualize(image2)
         image1, image2 = self.transform(image1, image2)
-        visualize(image1.permute(1, 2, 0))
-        visualize(image2.permute(1, 2, 0))
+        # visualize(image1.permute(1, 2, 0))
+        # visualize(image2.permute(1, 2, 0))
         return image1, image2
 
     def __len__(self) -> int:
@@ -299,6 +318,7 @@ def main():
     # save_bounding_boxes_for_barlow_twins(pair_meta_path)
     # refine_barlow_pairs_boxes(pair_meta_path)
     debug_refined_barlow_pairs_boxes
+
 
 if __name__ == '__main__':
     main()
